8b3bd587b7cdaa07fa9ec9222d801f36ea84ef8f
Roman Asadov <roma-asadov@mail.ru>
2019-10-07 20:15:01 +0300

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {FirstTestComponent} from './first-test/first-test.component';
 import {StartingMonitorComponent} from './starting-monitor/starting-monitor.component';
+import {SecondTestComponent} from './second-test/second-test.component';
 
 const routes: Routes = [
   { path: '', component: StartingMonitorComponent},
   { path: 'firstTest', component: FirstTestComponent},
-  { path: 'secondTest', component: StartingMonitorComponent}
+  { path: 'secondTest', component: SecondTestComponent}
 ];
 
 @NgModule({
